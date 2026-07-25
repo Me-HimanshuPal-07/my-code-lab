dce568c28cf76d6fd6a7b60e158990757a579152
Himanshu Pal <palshabhimanshu@gmail.com>
2026-07-25 10:00:21 +0530

    Add variables.js

---
+++ full-stack-ai/dsa/day_01/variables.js
@@ -0,0 +1,17 @@
+// const n = 100;
+// console.log(n);
+
+// let n = 100;
+// n = 10;
+// console.log(n);
+
+// var n = 100;
+// n = 10;
+// console.log(n);
+
+//Difference Between let or const
+//console.log(a);
+//let a = 10;//ReferenceError: Cannot access 'a' before initialization : TDZ(Temporal Dead Zone)
+
+// console.log(a);
+// var a = 100; //undefined : Hoisting
