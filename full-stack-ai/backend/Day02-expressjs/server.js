492e9d7518c8526ca6b315c49ca9be9320e75946
Himanshu Pal <palshabhimanshu@gmail.com>
2026-08-21 15:47:09 +0530

    Express js started

--- full-stack-ai/backend/Day02-expressjs/server.js
+++ full-stack-ai/backend/Day02-expressjs/server.js
@@ -1,18 +1,38 @@
+// Topics:
+// 1. Understanding ExpressJS.
+// 2. Why we use express instead of http sever.
+// 3. Understand what are the API/.
+// 4. Rest Apis: GET, POST, PUT/PATCH, DELETE.API
+// 5. Create our first Apis.
 
-// Topics: 
-            // 1. Understanding ExpressJS.
-            // 2. Why we use express instead of http sever.
-            // 3. Understand what are the API/.
-            // 4. Rest Apis: GET, POST, PUT/PATCH, DELETE.API
-            // 5. Create our first Apis.
+// const http = require("http");
+// const server = http.createServer((req, res) => {
+//    if (req.url === "/home"){
+//     res.end("Hello Home.");
+//    }
 
-const http = require("http");
-const server = http.createServer((req, res) => {
-    console.log("Hello Himanshu.");
-    res.end("Mujhe tumhari request mil gyi , I LOve U.")
+//    if (req.url === "/cart"){
+//     res.end("Hello cart");
+//    }
+//    if (req.url === "/about"){
+//     res.end("Hello about.");
+//    }
+//    if (req.url === "/job"){
+//     res.end("Hello  job.");
+//    }
+//     //this approach is repeatable this approach is not good for api's so we use express js.
+// });
+
+// server.listen(3000, () =>{
+//     console.log("Hello, i am running.")
+// });
+
+const express = require("express");
+const app = express();
+app.get("/", (req, res) => {
+  res.send("hello i am also here in home");
 });
-
-server.listen('3000', () =>{
-    console.log("Bro, Himanshu Server is ON.");
-    
+const port = 3000;
+app.listen(port, () => {
+  console.log(`Server is running on ${port}`);
 });
